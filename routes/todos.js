440c765593d062d68f14cb233fea84b959d913e8
Andy Wyllie <andy45386@gmail.com>
2018-04-11 05:51:51 +0000

    adding show route

--- routes/todos.js
+++ routes/todos.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 
+// index route for todos
 router.get('/', function(req, res){
     db.Todo.find()
     .then(function(todos){
@@ -12,8 +13,36 @@ router.get('/', function(req, res){
     })
 });
 
+// create route for todos
 router.post('/', function(req, res){
-    res.send('post route');
+    db.Todo.create(req.body)
+    .then(function(newTodo){
+        res.status(201).json(newTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
 });
+
+// show route
+router.get('/:todoId', function(req, res){
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.josn(foundTodo)
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+});
+
+
+// update route
+// router.put('/:todoId/edit', function(req, res){
+//     db.Todo.findByIdAndUpdate()
+// })
+
+
+// delete route
+
 
 module.exports = router;
